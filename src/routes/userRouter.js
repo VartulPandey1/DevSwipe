ae8d8ad80dba814c9b10aa1af87ab5f2caf55b22
Vartul Pandey <vartulpandey875@gmail.com>
2024-11-29 23:08:57 +0530

    bug fixed where user can see himself in the feed list

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,15 +2,20 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middleware/auth.js");
 const connection = require("../model/connect.js");
-const User = require("../model/user.js")
+const User = require("../model/user.js");
 userRouter.get("/user/requests", userAuth, async (req, res) => {
   try {
     const userId = req.user._id;
-    const data = await connection.find({
-      connectionRequestTo: userId,
-      status: "interested",
-    }).populate("connectionRequestFrom", "firstName lastName age photoURL skills gender about");
-    res.send(data)
+    const data = await connection
+      .find({
+        connectionRequestTo: userId,
+        status: "interested",
+      })
+      .populate(
+        "connectionRequestFrom",
+        "firstName lastName age photoURL skills gender about"
+      );
+    res.send(data);
   } catch (err) {
     res.send(err.message);
   }
@@ -26,8 +31,14 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
           { connectionRequestFrom: userId, status: "accepted" },
         ],
       })
-      .populate("connectionRequestTo", "firstName lastName age photoURL skills gender about")
-      .populate("connectionRequestFrom", "firstName lastName age photoURL skills gender about");
+      .populate(
+        "connectionRequestTo",
+        "firstName lastName age photoURL skills gender about"
+      )
+      .populate(
+        "connectionRequestFrom",
+        "firstName lastName age photoURL skills gender about"
+      );
     res.send(data);
   } catch (err) {
     res.send(err.message);
@@ -35,34 +46,40 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 });
 
 userRouter.get("/user/feed", userAuth, async (req, res) => {
-    try {
-        let {page,limit} = req.query
-        page?page=page:page=1
-        limit?limit<=10?limit:limit=10:limit=5
-        const skip = (page-1)*limit
-      const userId = req.user._id;
-      const userWithConnection = await connection
-        .find({
-          $or: [
-            { connectionRequestTo: userId},
-            { connectionRequestFrom: userId},
-          ],
-        })
-        .populate("connectionRequestTo", "_id")
-        .populate("connectionRequestFrom", "_id");
-      const notFeedUserSet = new Set()
-      userWithConnection.forEach((data)=>{
-        notFeedUserSet.add(data.connectionRequestTo._id)
-        notFeedUserSet.add(data.connectionRequestFrom._id)
-    })
-    const notFeedUserArray = [...notFeedUserSet]
+  try {
+    let { page, limit } = req.query;
+    page ? (page = page) : (page = 1);
+    limit ? (limit <= 10 ? limit : (limit = 10)) : (limit = 5);
+    const skip = (page - 1) * limit;
+    const userId = req.user._id;
+    const userWithConnection = await connection
+      .find({
+        $or: [
+          { connectionRequestTo: userId },
+          { connectionRequestFrom: userId },
+        ],
+      })
+      .populate("connectionRequestTo", "_id")
+      .populate("connectionRequestFrom", "_id");
+    const notFeedUserSet = new Set();
+    userWithConnection.forEach((data) => {
+      notFeedUserSet.add(data.connectionRequestTo._id);
+      notFeedUserSet.add(data.connectionRequestFrom._id);
+    });
+    const notFeedUserArray = [...notFeedUserSet];
+    console.log(notFeedUserArray);
     const data = await User.find({
-        _id:{$nin : notFeedUserArray}
-    }).skip(skip).limit(limit)
+      $and: [
+        { _id: { $nin: notFeedUserArray } },
+        { _id: { $nin: userId } },
+      ],
+    })
+      .skip(skip)
+      .limit(limit);
 
-      res.send(data);
-    } catch (err) {
-      res.send(err.message);
-    }
-  });
+    res.send(data);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
 module.exports = userRouter;
